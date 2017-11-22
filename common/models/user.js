ee59513008e3a6221d7fd9a23a930870dbcb7e04
Sardor Isakov <sisardor@gmail.com>
2017-11-21 18:03:34 -0800

    minor update

--- common/models/user.js
+++ common/models/user.js
@@ -1,5 +1,50 @@
 'use strict';
-
+var Recaptcha = require('express-recaptcha');
+//import Recaptcha from 'express-recaptcha'
+var recaptcha = new Recaptcha('SITE_KEY', '6LdM3zkUAAAAAL1d8fzWMiEoSBA7iwFgxIc-2Fmp');
 module.exports = function(User) {
+  //send verification email after registration
+  User.observe('before save', function (ctx, next) {
+    console.log(ctx)
+    try {
+      if (true) {
+        throw {
+          name: 'Access token error',
+          message: 'Recaptcha note selected.',
+          status: 401,
+          statusCode: 401
+        }
+      }
+    } catch (e) {
+      return next(e)
+    } finally {
 
+    }
+    next()
+  })
+  // User.afterRemote('create', function(context, user, next) {
+  //   var options = {
+  //     type: 'email',
+  //     to: user.email,
+  //     from: 'admin@atomex.io',
+  //     subject: 'Thanks for registering.',
+  //     template: path.resolve(__dirname, '../../server/views/verify.ejs'),
+  //     redirect: '/verified',
+  //     user: user
+  //   };
+  //
+  //   user.verify(options, function(err, response) {
+  //     if (err) {
+  //       User.deleteById(user.id);
+  //       return next(err);
+  //     }
+  //     context.res.render('response', {
+  //       title: 'Signed up successfully',
+  //       content: 'Please check your email and click on the verification link ' +
+  //           'before logging in.',
+  //       redirectTo: '/',
+  //       redirectToLinkText: 'Log in'
+  //     });
+  //   });
+  // });
 };
